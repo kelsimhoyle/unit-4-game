c81d2f0e4700b36e497878847f7f73ed5f0db55a
Kelsi H <kelsimhoyle@gmail.com>
2019-05-08 10:04:12 -0600

    pictures are added to the page through javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,32 +2,61 @@ $(document).ready(function () {
     var targetNum;
     var runningTotal = 0;
     var wins = 0;
-    var losses = 0;
-    var crystalOneNum;
-    var crystalTwoNum;
-    var crystalThreeNum;
-    var crystalFourNum;
+    var losses = 0
     var gameOver = false;
+
+    // objects for the crystals and their attributes
+    var crystalsList = [
+        {
+            crystal: "one",
+            value: 0,
+            src: "assets/images/blueGemstone.jpg",
+        },
+        {
+            crystal: "two",
+            value: 0,
+            src: "assets/images/greenGemstone.jpg",
+        },
+        {
+            crystal: "three",
+            value: 0,
+            src: "assets/images/purpleGemstone.jpeg"
+        },
+        {
+            crystal: "four",
+            value: 0,
+            src: "assets/images/rubyGemstone.jpg"
+        },
+    ]
 
     function randomCrystalNum() {
         return Math.floor(Math.random() * 12) + 1;
     }
 
     function createNumbers() {
-        crystalOneNum = randomCrystalNum();
-        crystalTwoNum = randomCrystalNum();
-        crystalThreeNum = randomCrystalNum();
-        crystalFourNum = randomCrystalNum();
+        for (var i = 0; i < crystalsList.length; i++) {
+            crystalsList[i].value = randomCrystalNum();
+        }
         targetNum = Math.floor(Math.random() * 100) + 20;
+    }
+
+    function creatCrystals() {
+        for (var i = 0; i < crystalsList.length; i++ ) {
+            // create a div for the crystals to go to with a specific  value
+            var crystalDiv = $("<div>").addClass("crystal").attr("id", crystalsList[i].crystal)
+            crystalDiv.attr("data-value", crystalsList[i].value);
+            $("#crystals-display").append(crystalDiv);
+            // put the picture in the div 
+            var crystalPic = $("<img>");
+            crystalPic.attr("src", crystalsList[i].src);
+            $(crystalDiv).append(crystalPic);
+        }
     }
 
     function startGame() {
         $("#winning-div, #losing-div").hide();
         createNumbers();
-        $("#crystal1").attr("data-value", crystalOneNum);
-        $("#crystal2").attr("data-value", crystalTwoNum);
-        $("#crystal3").attr("data-value", crystalThreeNum);
-        $("#crystal4").attr("data-value", crystalFourNum);
+        creatCrystals();
         runningTotal = 0;
         gameOver = false;
         $("#target-number").text(targetNum);
@@ -40,15 +69,15 @@ $(document).ready(function () {
 
     // when the crystal is clicked, get the data-value attr and add it to the runningTotal
     $(".crystal").on("click", function () {
-        if(gameOver) return;
-        
+        if (gameOver) return;
+
         if (runningTotal < targetNum) {
             var crystalNum = parseInt($(this).attr("data-value"));
             runningTotal += crystalNum;
             console.log(runningTotal);
             $("#running-total").text(runningTotal);
-        } 
-        
+        }
+
         if (runningTotal > targetNum) {
             losses++;
             $("#losing-div").show();
@@ -60,7 +89,7 @@ $(document).ready(function () {
         }
     });
 
-    $(".play-again").on("click", function() {
+    $(".play-again").on("click", function () {
         startGame();
     });
 
